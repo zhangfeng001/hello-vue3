2929e5acc3cdbdd5e4417c8927413a3ccced21e7
zhangfeng001 <1121221073@qq.com>
2021-03-18 20:26:26 +0800

    add markdown

---
+++ src/components/beforeUpdate.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>等待数据变化{{count}}</div>
+</template>
+<script lang="ts">
+import { defineComponent ,onBeforeUpdate, computed, } from 'vue'
+import { useStore } from '../store'
+
+export default defineComponent({
+    setup() {
+        // 获取store里的值
+      const store = useStore()
+      const count = computed(() => store.state.userModule.count)
+        onBeforeUpdate( () => {
+            console.log('this is beforeUpdate comp')
+            return false;
+        })
+        return {
+           count 
+        }
+    },
+})
+</script>
