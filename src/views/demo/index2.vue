0dd1909f1fae58d70710022bed5e224741b76170
liuZongYi <er345ssdlh@163.com>
2021-03-29 11:30:13 +0800

    feat: 整理

--- src/views/demo/index2.vue
+++ src/views/demo/index2.vue
@@ -4,20 +4,15 @@
  * @Author: lzy
  * @Date: 2021-03-29 10:43:52
  * @LastEditors: Andy
- * @LastEditTime: 2021-03-29 11:26:46
+ * @LastEditTime: 2021-03-29 11:29:30
 -->
 <template>
+  <div>测试demo页</div>
 </template>
 <script>
 import {getCurrentInstance,ref} from 'vue';
 export default {
   setup(props,content){
-        const num =ref(0)
-        const name =ref('刘宗怡')
-        const ctx = getCurrentInstance()
-        console.log(props)
-        console.log(content)
-        console.log(ctx.appContext.config.globalProperties) //ctx包含content
         return {
         }
     }
