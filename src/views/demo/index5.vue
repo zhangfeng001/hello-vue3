d59437d4aff9a7577503ca269c687823c236470d
liuZongYi <er345ssdlh@163.com>
2021-04-07 17:04:51 +0800

    feat:下载图片工作

--- src/views/demo/index5.vue
+++ src/views/demo/index5.vue
@@ -36,7 +36,7 @@
 </template>
 <script>
 export default {
-  name: "canvas1",
+  name: "canvas",
   data() {
     return {
       ctx: "",
@@ -118,7 +118,7 @@ export default {
         }else if (e.keyCode == 40 && this.tagObj.recs[this.tagObj.index].y <(this.$refs.tagcanvas.height-this.tagObj.recs[this.tagObj.index].h)) {
           this.tagObj.recs[this.tagObj.index].y++
         }else {
-          alert('到边边了')
+          // alert('到边边了')
         }
         this.clearCanvas(this.$refs.tagcanvas, this.ctx); // 边移动边清除
         this.drawRuler(this.$refs.tagcanvas, this.ctx, e); // 交叉辅助线
@@ -142,6 +142,34 @@ export default {
     },
     handleCopy(){
       let imgData = this.tagObj.url; //填写你的base64
+      this.downloadFile('canvas.png', imgData);
+    },
+    //下载
+    downloadFile(fileName, content) {
+      let aLink = document.createElement('a');
+      let blob = this.base64ToBlob(content); //new Blob([content]);
+
+      let evt = document.createEvent("HTMLEvents");
+      evt.initEvent("click", true, true);//initEvent 不加后两个参数在FF下会报错  事件类型，是否冒泡，是否阻止浏览器的默认行为
+      aLink.download = fileName;
+      aLink.href = URL.createObjectURL(blob);
+
+      // aLink.dispatchEvent(evt);
+      aLink.click()
+    },
+    //base64转blob
+    base64ToBlob(code) {
+      let parts = code.split(';base64,');
+      let contentType = parts[0].split(':')[1];
+      let raw = window.atob(parts[1]);
+      let rawLength = raw.length;
+
+      let uInt8Array = new Uint8Array(rawLength);
+
+      for (let i = 0; i < rawLength; ++i) {
+        uInt8Array[i] = raw.charCodeAt(i);
+      }
+      return new Blob([uInt8Array], {type: contentType});
     },
     getOffsetLeft(obj) {
       //获取元素距离浏览器左侧距离
@@ -263,6 +291,7 @@ export default {
       const canvasPosition = this.getCanvasPosition(e,this.offset,this.scale)
       this.tagObj.x = canvasPosition.x; // 每次落脚的落脚点为轴线圆心
       this.tagObj.y = canvasPosition.y; // 每次落脚的落脚点为轴线圆心
+      // 每执行依次beginpath(),检验就会变成之后的,辅助线，区域框，图形都在移动中重复的画着，故这里不适用
       // console.log(this.ctx.isPointInPath(canvasPosition.y, canvasPosition.y))
       //得到落点所在框的序数
       this.tagObj.index = this.getEventIndex(
